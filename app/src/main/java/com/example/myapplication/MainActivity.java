e373be4a97512997acfdd1df439674412fd64883
Akshay singh <akshays6325@gmail.com>
2023-07-11 22:20:04 -0400

    changes made : same email id cannot be used for creating another account. sign up form form validation. logout button modification

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.myapplication;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -42,41 +43,7 @@ public class MainActivity extends AppCompatActivity {
         buttonSignIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Retrieve the entered email and password
-                String email = editTextEmail.getText().toString().trim();
-                String password = editTextPassword.getText().toString().trim();
-
-                // Query the database to find a user with matching email and password
-                Query query = usersRef.orderByChild("email").equalTo(email);
-                query.addListenerForSingleValueEvent(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(DataSnapshot dataSnapshot) {
-                        boolean isUserFound = false;
-
-                        for (DataSnapshot userSnapshot : dataSnapshot.getChildren()) {
-                            User user = userSnapshot.getValue(User.class);
-                            if (user != null && user.getPassword().equals(password)) {
-                                // User authentication successful
-                                isUserFound = true;
-                                // Add your code here to handle the authenticated user
-                                Toast.makeText(MainActivity.this, "Sign in successful!", Toast.LENGTH_SHORT).show();
-                                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
-                                startActivity(intent);
-                            }
-                        }
-
-                        if (!isUserFound) {
-                            // No matching user or incorrect password
-                            Toast.makeText(MainActivity.this, "Invalid email or password!", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-
-                    @Override
-                    public void onCancelled(DatabaseError databaseError) {
-                        // Handle error
-                        Toast.makeText(MainActivity.this, "Error occurred: " + databaseError.getMessage(), Toast.LENGTH_SHORT).show();
-                    }
-                });
+                signIn();
             }
         });
 
@@ -87,21 +54,61 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
         buttonAdmin = findViewById(R.id.buttonAdmin);
         buttonAdmin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Handle Admin button click
-                // Add your logic here
-                buttonAdmin.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Intent intent = new Intent(MainActivity.this, AdminLoginActivity.class);
-                        startActivity(intent);
-                    }
-                });
+                Intent intent = new Intent(MainActivity.this, AdminLoginActivity.class);
+                startActivity(intent);
             }
         });
+    }
 
+    private void signIn() {
+        String email = editTextEmail.getText().toString().trim();
+        String password = editTextPassword.getText().toString().trim();
+
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(this, "Please enter your email", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(this, "Please enter your password", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // Query the database to find a user with matching email and password
+        Query query = usersRef.orderByChild("email").equalTo(email);
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                boolean isUserFound = false;
+
+                for (DataSnapshot userSnapshot : dataSnapshot.getChildren()) {
+                    User user = userSnapshot.getValue(User.class);
+                    if (user != null && user.getPassword().equals(password)) {
+                        // User authentication successful
+                        isUserFound = true;
+                        // Add your code here to handle the authenticated user
+                        Toast.makeText(MainActivity.this, "Sign in successful!", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                        startActivity(intent);
+                    }
+                }
+
+                if (!isUserFound) {
+                    // No matching user or incorrect password
+                    Toast.makeText(MainActivity.this, "Invalid email or password!", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                // Handle error
+                Toast.makeText(MainActivity.this, "Error occurred: " + databaseError.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/myapplication/SignupActivity.java
+++ app/src/main/java/com/example/myapplication/SignupActivity.java
@@ -6,10 +6,15 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 public class SignupActivity extends AppCompatActivity {
 
@@ -41,26 +46,59 @@ public class SignupActivity extends AppCompatActivity {
                 String email = editTextEmail.getText().toString().trim();
                 String password = editTextPassword.getText().toString().trim();
 
-                // Validate the inputs
+                // Perform form validation
                 if (name.isEmpty() || email.isEmpty() || password.isEmpty()) {
                     Toast.makeText(SignupActivity.this, "Please fill in all fields", Toast.LENGTH_SHORT).show();
                     return;
                 }
 
-                // Create a new user object
-                User user = new User(name, email, password);
+                if (!isValidEmail(email)) {
+                    Toast.makeText(SignupActivity.this, "Invalid email format", Toast.LENGTH_SHORT).show();
+                    return;
+                }
 
-                // Generate a unique user ID
-                String userId = usersRef.push().getKey();
+                if (password.length() < 6) {
+                    Toast.makeText(SignupActivity.this, "Password must be at least 6 characters", Toast.LENGTH_SHORT).show();
+                    return;
+                }
 
-                // Store the user data in the database using the generated user ID
-                usersRef.child(userId).setValue(user);
+                // Check if the email already exists in the database
+                Query query = usersRef.orderByChild("email").equalTo(email);
+                query.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        if (dataSnapshot.exists()) {
+                            // Email already in use
+                            Toast.makeText(SignupActivity.this, "Email already in use", Toast.LENGTH_SHORT).show();
+                        } else {
+                            // Create a new user object
+                            User user = new User(name, email, password);
 
-                // Show a success message
-                Toast.makeText(SignupActivity.this, "Sign up successful!", Toast.LENGTH_SHORT).show();
+                            // Generate a unique user ID
+                            String userId = usersRef.push().getKey();
 
-                // Optional: You may want to redirect the user to the login screen or perform other actions here.
+                            // Store the user data in the database using the generated user ID
+                            usersRef.child(userId).setValue(user);
+
+                            // Show a success message
+                            Toast.makeText(SignupActivity.this, "Sign up successful!", Toast.LENGTH_SHORT).show();
+
+                            // Optional: You may want to redirect the user to the login screen or perform other actions here.
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+                        // Handle error
+                        Toast.makeText(SignupActivity.this, "Error occurred: " + databaseError.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
             }
         });
+    }
+
+    private boolean isValidEmail(String email) {
+        String emailRegex = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$";
+        return email.matches(emailRegex);
     }
 }
